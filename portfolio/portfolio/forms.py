7b30011cdc43a632195024e210466286eccdca57
Aditya vikram <adivikram2222@gmail.com>
2021-10-30 11:41:04 +0530

    renew

--- portfolio/portfolio/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms 
-
-class contactformemail(forms.form):
-    fromeemail=forms.EmailField(required=True)
-    subject=form.Charfield(required=True)
-    message=form.charfiled(required=True)
