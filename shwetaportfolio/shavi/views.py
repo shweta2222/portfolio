653bbc856e756823f0d95e68147c4ec6f7240495
Aditya vikram <adivikram2222@gmail.com>
2021-10-30 15:33:39 +0530

    updated

--- shwetaportfolio/shavi/views.py
+++ shwetaportfolio/shavi/views.py
@@ -1,5 +1,20 @@
+from django.core.checks import messages
 from django.shortcuts import render
+from shavi.models import Contact
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    if request.method=="POST":
+        contact=Contact()
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        message=request.POST.get('message')
+        contact.name=name
+        contact.email=email
+        contact.subject=subject
+        contact.message=message
+        contact.save()
+        return HttpResponse("<h1>THANKS FOR CONTACT US </h1>")
     return render(request,'index.html')
