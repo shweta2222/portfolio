233009693ebd1acfcdf5176ebf1f4ee2ba22cf2d
Aditya vikram <adivikram2222@gmail.com>
2022-01-14 23:09:02 +0530

    update

--- shwetaportfolio/shavi/models.py
+++ shwetaportfolio/shavi/models.py
@@ -4,9 +4,9 @@ from django import forms
 from django.contrib.auth.models import User
 # Create your models here.
 class Contact(models.Model):
-    name=models.CharField(max_length=200,null=True)
-    email=models.EmailField(max_length=200,null=True,blank=True)
-    subject=models.TextField(null=True,blank=True)
-    message=models.TextField(null=True,blank=True)
+    name=models.CharField(max_length=20,null=True,blank=True)
+    email=models.EmailField(max_length=20,null=True,blank=True)
+    subject=models.CharField(max_length=20,null=True,blank=True)
+    message=models.TextField(max_length=200,null=True,blank=True)
     def __str__(self):
         return self.name
